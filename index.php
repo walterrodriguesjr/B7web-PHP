738f44ea0abb9adc0488867208863537fd589b31
Walter <you@example.com>
2023-05-15 13:24:36 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
-<?php 
+<?php
 
 $array = [
     'nome' => 'Walter',
     'idade' => 37,
     'empresa' => 'PMPR',
     'cor' => 'preto',
-    'profissão' => 'Desenvolvedor'
+    'profissão' => 'Desenvolvedor',
 ];
 
 $chaves = array_keys($array);
 $valores = array_values($array);
-print_r($chaves);
-echo "<br>";
-print_r($valores);
 
+?>
+
+<table border="1">
+    <tr>
+        <?php foreach ($chaves as $chave) : ?>
+            <th><?php echo $chave; ?></th>
+        <?php endforeach; ?>
+
+    </tr>
+    <tr>
+        <?php foreach ($valores as $valor) : ?>
+            <td><?php echo $valor;  ?></td>
+        <?php endforeach; ?>
+    </tr>
+
+</table>
