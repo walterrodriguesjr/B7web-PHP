4e2268c71793460684b5565b9d4d354d7cf0ed9b
Walter <you@example.com>
2023-05-19 13:26:58 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php 
 
+class Post {
+    public $likes = 0;       //vai receber números
+    public $comments = [];   // vai receber um array de comentários
+    public $author;         // vai receber uma string
+}
+
+$postagem1 = new Post();
+$postagem1->likes = 5;
+$postagem1->comments[0] = "Comentário teste";
+$postagem1->author = "Walter";
+
+echo "<pre>";
+print_r($postagem1);
+echo "</pre>";
